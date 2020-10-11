638df1005c37b09bfbd606163bebab6207d52abd
unknown <seambark@gmail.com>
2020-10-11 23:47:54 +0900

    js 작업 및 아이템 데이터 삽입

---
+++ assets/js/items.js
@@ -0,0 +1,144 @@
+let items = [
+    {
+        type: 'doll',
+        name: '베베',
+        url: '/assets/images/doll/doll_01.png'
+    },
+    {
+        type: 'doll',
+        name: '베시',
+        url: '/assets/images/doll/doll_02.png'
+    },
+    {
+        type: 'clothes',
+        name: '사파리',
+        url: '/assets/images/body/clothes/clothes_01.png',
+        setItem: ''
+    },
+    {
+        type: 'clothes',
+        name: '물망초',
+        url: '/assets/images/body/clothes/clothes_02.png',
+        setItem: ''
+    },
+    {
+        type: 'clothes',
+        name: '클로버',
+        url: '/assets/images/body/clothes/clothes_03.png',
+        setItem: ''
+    },
+    {
+        type: 'clothes',
+        name: '튤립',
+        url: '/assets/images/body/clothes/clothes_04.png',
+        setItem: ''
+    },
+    {
+        type: 'clothes',
+        name: '들꽃',
+        url: '/assets/images/body/clothes/clothes_05.png',
+        setItem: ''
+    },
+    {
+        type: 'clothes',
+        name: '들장미',
+        url: '/assets/images/body/clothes/clothes_06.png',
+        setItem: ''
+    },
+    {
+        type: 'clothes',
+        name: '루바슈카',
+        url: '/assets/images/body/clothes/clothes_07.png',
+        setItem: '러시아'
+    },
+    {
+        type: 'clothes',
+        name: '물결',
+        url: '/assets/images/body/clothes/clothes_08.png',
+        setItem: ''
+    },
+    {
+        type: 'clothes',
+        name: '은방울',
+        url: '/assets/images/body/clothes/clothes_09.png',
+        setItem: ''
+    },
+    {
+        type: 'clothes',
+        name: '벚꽃',
+        url: '/assets/images/body/clothes/clothes_10.png',
+        setItem: ''
+    },
+    {
+        type: 'accessory',
+        name: '물망초가방',
+        url: '/assets/images/body/accessory/bag_01.png',
+        setItem: ''
+    },
+    {
+        type: 'accessory',
+        name: '클로버가방',
+        url: '/assets/images/body/accessory/bag_02.png',
+        setItem: ''
+    },
+    {
+        type: 'accessory',
+        name: '들꽃가방',
+        url: '/assets/images/body/accessory/bag_03.png',
+        setItem: ''
+    },
+    {
+        type: 'accessory',
+        name: '들장미가방',
+        url: '/assets/images/body/accessory/bag_04.png',
+        setItem: ''
+    },
+    {
+        type: 'accessory',
+        name: '채송화바구니',
+        url: '/assets/images/body/accessory/flower_01.png',
+        setItem: ''
+    },
+    {
+        type: 'accessory',
+        name: '튤립바구니',
+        url: '/assets/images/body/accessory/flower_02.png',
+        setItem: ''
+    },
+    {
+        type: 'accessory',
+        name: '장미바구니',
+        url: '/assets/images/body/accessory/flower_03.png',
+        setItem: ''
+    },
+    {
+        type: 'accessory',
+        name: '아네모네꽃다발',
+        url: '/assets/images/body/accessory/flower_04.png',
+        setItem: ''
+    },
+    {
+        type: 'accessory',
+        name: '분홍양산',
+        url: '/assets/images/body/accessory/others_01.png',
+        setItem: ''
+    },
+    {
+        type: 'accessory',
+        name: '푸른양산',
+        url: '/assets/images/body/accessory/others_02.png',
+        setItem: ''
+    },
+    {
+        type: 'accessory',
+        name: '인형',
+        url: '/assets/images/body/accessory/others_03.png',
+        setItem: ''
+    },
+    {
+        type: 'accessory',
+        name: '부채',
+        url: '/assets/images/body/accessory/others_04.png',
+        setItem: ''
+    }
+]
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,81 @@
+let itemArea = document.querySelector('#itemArea');
 let tab = document.querySelector('.tab');
-let tabLi = document.querySelector('li');
+let tabLi = document.querySelector('.tab li');
 let tabActive;
+let listActive;
 
-tab.addEventListener('click', onTab)
+let itemList = document.querySelector('.itemList');
+
+tab.addEventListener('click', onTab);
+// itemList.addEventListener('click', onItem)
+
+function onItem() {
+  let item = e.target;
+  if (item.tagName === 'BUTTON') {
+    item.querySelector('img').getAttribute('src');
+  }
+}
+
+function tabCheck(active) {
+  let creatSpan = document.createElement('span');
+
+  tabActive === undefined && tabLi.querySelector('a span').remove()
+
+  if (tabActive) {
+    let check = tabActive.querySelector('span');
+    check.remove();
+  }
+
+  active.appendChild(creatSpan);
+  creatSpan.classList.add('blind');
+  creatSpan.innerText = '현재 페이지';
+}
+
+function creatitemListItem(type) {
+  let tabCont = document.querySelector('.tabCont');
+  let ul = document.createElement('ul');
+
+  tabCont.appendChild(ul)
+  ul.classList.add('itemList');
+
+  ul.classList.add(`tab${type[0].type[0].toUpperCase()}${type[0].type.slice(1)}`);
+  // ul.classList.add(`item${type[0].type[0].toUpperCase()}${type[0].type.slice(1)}`)
+
+  for (let i = 0; i < type.length; i++) {
+    let creatLi = document.createElement('li');
+    ul.appendChild(creatLi);
+    creatLi.innerHTML = `
+      <button class="item">
+        <img src="${type[i].url}" alt="${type[i].name}">
+        <span class="nameTag">${type[i].name}</span>
+      </button>
+    `
+  }
+}
+
+function dataItemsList() {
+  let doll = items.filter(itemsFilter => itemsFilter.type.includes('doll'))
+  creatitemListItem(doll)
+
+  let clothes = items.filter(itemsFilter => itemsFilter.type.includes('clothes'))
+  creatitemListItem(clothes)
+
+  let accessory = items.filter(itemsFilter => itemsFilter.type.includes('accessory'))
+  creatitemListItem(accessory)
+}
+
+function dataActive(id) {
+  let doll = document.querySelector('.tabDoll')
+  let clothes = document.querySelector('.tabClothes')
+
+  if (id === 'tabDoll') {
+    doll.style.display = 'block'
+  } else if (id === 'tabClothes') {
+    clothes.style.display = 'block'
+  }
+}
+
+dataItemsList()
 
 function onTab(e) {
   let target = e.target;
@@ -15,19 +88,11 @@ function onTab(e) {
     tabCheck(target)
     tabActive = target;
   }
-}
 
-function tabCheck(active) {
-  let creatSpan = document.createElement('span');
-
-  tabActive === undefined && tabLi.querySelector('a span').remove()
-
-  if (tabActive) {
-    let check = tabActive.querySelector('span');
-    check.remove()
+  let listCheck = document.querySelectorAll('.itemList')
+  for (let i = 0; i < listCheck.length; i++) {
+    listCheck[i].style.display = 'none'
   }
 
-  active.appendChild(creatSpan)
-  creatSpan.classList.add('blind')
-  creatSpan.innerText = '현재 페이지'
+  dataActive(target.id)
 }
