14c1b419802b13946ac6fd81df630e63286fcee0
unknown <seambark@gmail.com>
2020-10-13 09:03:28 +0900

    이미지 파일 추가 및 README에 출처 기입,  items.js에 이미지 내용 추가

--- assets/js/items.js
+++ assets/js/items.js
@@ -2,143 +2,279 @@ let items = [
     {
         type: 'doll',
         name: '베베',
-        url: '/assets/images/doll/doll_01.png'
+        url: '/assets/images/doll/doll_01.png',
+        urlThmb: '/assets/images/doll/doll_01_thmb.png'
     },
     {
         type: 'doll',
         name: '베시',
-        url: '/assets/images/doll/doll_02.png'
+        url: '/assets/images/doll/doll_02.png',
+        urlThmb: '/assets/images/doll/doll_02_thmb.png'
     },
     {
         type: 'clothes',
         name: '사파리',
         url: '/assets/images/body/clothes/clothes_01.png',
+        urlThmb: '/assets/images/body/clothes/clothes_01_thmb.png',
         setItem: ''
     },
     {
         type: 'clothes',
         name: '물망초',
         url: '/assets/images/body/clothes/clothes_02.png',
+        urlThmb: '/assets/images/body/clothes/clothes_02_thmb.png',
         setItem: ''
     },
     {
         type: 'clothes',
         name: '클로버',
         url: '/assets/images/body/clothes/clothes_03.png',
+        urlThmb: '/assets/images/body/clothes/clothes_03_thmb.png',
         setItem: ''
     },
     {
         type: 'clothes',
         name: '튤립',
         url: '/assets/images/body/clothes/clothes_04.png',
+        urlThmb: '/assets/images/body/clothes/clothes_04_thmb.png',
         setItem: ''
     },
     {
         type: 'clothes',
         name: '들꽃',
         url: '/assets/images/body/clothes/clothes_05.png',
+        urlThmb: '/assets/images/body/clothes/clothes_05_thmb.png',
         setItem: ''
     },
     {
         type: 'clothes',
         name: '들장미',
         url: '/assets/images/body/clothes/clothes_06.png',
+        urlThmb: '/assets/images/body/clothes/clothes_06_thmb.png',
         setItem: ''
     },
     {
         type: 'clothes',
         name: '루바슈카',
         url: '/assets/images/body/clothes/clothes_07.png',
+        urlThmb: '/assets/images/body/clothes/clothes_07_thmb.png',
         setItem: '러시아'
     },
     {
         type: 'clothes',
         name: '물결',
         url: '/assets/images/body/clothes/clothes_08.png',
+        urlThmb: '/assets/images/body/clothes/clothes_08_thmb.png',
         setItem: ''
     },
     {
         type: 'clothes',
         name: '은방울',
         url: '/assets/images/body/clothes/clothes_09.png',
+        urlThmb: '/assets/images/body/clothes/clothes_09_thmb.png',
         setItem: ''
     },
     {
         type: 'clothes',
         name: '벚꽃',
         url: '/assets/images/body/clothes/clothes_10.png',
+        urlThmb: '/assets/images/body/clothes/clothes_10_thmb.png',
         setItem: ''
     },
     {
         type: 'accessory',
         name: '물망초가방',
         url: '/assets/images/body/accessory/bag_01.png',
+        urlThmb: '/assets/images/body/accessory/bag_01_thmb.png',
         setItem: ''
     },
     {
         type: 'accessory',
         name: '클로버가방',
         url: '/assets/images/body/accessory/bag_02.png',
+        urlThmb: '/assets/images/body/accessory/bag_02_thmb.png',
         setItem: ''
     },
     {
         type: 'accessory',
         name: '들꽃가방',
         url: '/assets/images/body/accessory/bag_03.png',
+        urlThmb: '/assets/images/body/accessory/bag_03_thmb.png',
         setItem: ''
     },
     {
         type: 'accessory',
         name: '들장미가방',
         url: '/assets/images/body/accessory/bag_04.png',
+        urlThmb: '/assets/images/body/accessory/bag_04_thmb.png',
         setItem: ''
     },
     {
         type: 'accessory',
         name: '채송화바구니',
         url: '/assets/images/body/accessory/flower_01.png',
+        urlThmb: '/assets/images/body/accessory/flower_01_thmb.png',
         setItem: ''
     },
     {
         type: 'accessory',
         name: '튤립바구니',
         url: '/assets/images/body/accessory/flower_02.png',
+        urlThmb: '/assets/images/body/accessory/flower_02_thmb.png',
         setItem: ''
     },
     {
         type: 'accessory',
         name: '장미바구니',
         url: '/assets/images/body/accessory/flower_03.png',
+        urlThmb: '/assets/images/body/accessory/flower_03_thmb.png',
         setItem: ''
     },
     {
         type: 'accessory',
         name: '아네모네꽃다발',
         url: '/assets/images/body/accessory/flower_04.png',
+        urlThmb: '/assets/images/body/accessory/flower_04_thmb.png',
         setItem: ''
     },
     {
         type: 'accessory',
         name: '분홍양산',
         url: '/assets/images/body/accessory/others_01.png',
+        urlThmb: '/assets/images/body/accessory/others_01_thmb.png',
         setItem: ''
     },
     {
         type: 'accessory',
         name: '푸른양산',
         url: '/assets/images/body/accessory/others_02.png',
+        urlThmb: '/assets/images/body/accessory/others_02_thmb.png',
         setItem: ''
     },
     {
         type: 'accessory',
         name: '인형',
         url: '/assets/images/body/accessory/others_03.png',
+        urlThmb: '/assets/images/body/accessory/others_03_thmb.png',
         setItem: ''
     },
     {
         type: 'accessory',
         name: '부채',
         url: '/assets/images/body/accessory/others_04.png',
+        urlThmb: '/assets/images/body/accessory/others_04_thmb.png',
+        setItem: ''
+    },
+    {
+        type: 'hair',
+        name: '사파리캡',
+        url: '/assets/images/head/hat_01.png',
+        urlBack: '/assets/images/head/hat_01_back.png',
+        urlThmb: '/assets/images/head/hat_01_thmb.png',
+        setItem: ''
+    },
+    {
+        type: 'hair',
+        name: '레이스모자',
+        url: '/assets/images/head/hat_02.png',
+        urlBack: '/assets/images/head/hat_02_back.png',
+        urlThmb: '/assets/images/head/hat_02_thmb.png',
+        setItem: ''
+    },
+    {
+        type: 'hair',
+        name: '팔랑모자',
+        url: '/assets/images/head/hat_03.png',
+        urlBack: '/assets/images/head/hat_03_back.png',
+        urlThmb: '/assets/images/head/hat_03_thmb.png',
+        setItem: ''
+    },
+    {
+        type: 'hair',
+        name: '두건',
+        url: '/assets/images/head/hat_04.png',
+        urlBack: '/assets/images/head/hat_04_back.png',
+        urlThmb: '/assets/images/head/hat_04_thmb.png',
+        setItem: ''
+    },
+    {
+        type: 'hair',
+        name: '짚모자',
+        url: '/assets/images/head/hat_05.png',
+        urlThmb: '/assets/images/head/hat_05_thmb.png',
+        setItem: ''
+    },
+    {
+        type: 'hair',
+        name: '베레모',
+        url: '/assets/images/head/hat_06.png',
+        urlBack: '/assets/images/head/hat_06_back.png',
+        urlThmb: '/assets/images/head/hat_06_thmb.png',
+        setItem: ''
+    },
+    {
+        type: 'hair',
+        name: '붉은두건',
+        url: '/assets/images/head/hat_07.png',
+        urlThmb: '/assets/images/head/hat_07_thmb.png',
+        setItem: ''
+    },
+    {
+        type: 'hair',
+        name: '레이스베일',
+        url: '/assets/images/head/hat_08.png',
+        urlBack: '/assets/images/head/hat_08_back.png',
+        urlThmb: '/assets/images/head/hat_08_thmb.png',
+        setItem: ''
+    },
+    {
+        type: 'hair',
+        name: '화관베일',
+        url: '/assets/images/head/hat_09.png',
+        urlBack: '/assets/images/head/hat_09_back.png',
+        urlThmb: '/assets/images/head/hat_09_thmb.png',
+        setItem: ''
+    },
+    {
+        type: 'hair',
+        name: '리본',
+        url: '/assets/images/head/hair_accessory_01.png',
+        urlThmb: '/assets/images/head/hair_accessory_01_thmb.png',
+        setItem: ''
+    },
+    {
+        type: 'hair',
+        name: '올림머리',
+        url: '/assets/images/head/hair_01.png',
+        urlThmb: '/assets/images/head/hair_01_thmb.png',
+        setItem: ''
+    },
+    {
+        type: 'pet',
+        name: '닥스훈트',
+        url: '/assets/images/pet/pet_01.png',
+        urlThmb: '/assets/images/pet/pet_01_thmb.png',
+        setItem: ''
+    },
+    {
+        type: 'pet',
+        name: '푸들',
+        url: '/assets/images/pet/pet_02.png',
+        urlThmb: '/assets/images/pet/pet_02_thmb.png',
+        setItem: ''
+    },
+    {
+        type: 'pet',
+        name: '고양이',
+        url: '/assets/images/pet/pet_03.png',
+        urlThmb: '/assets/images/pet/pet_03_thmb.png',
+        setItem: ''
+    },
+    {
+        type: 'pet',
+        name: '파랑새',
+        url: '/assets/images/pet/pet_04.png',
+        urlThmb: '/assets/images/pet/pet_04_thmb.png',
         setItem: ''
     }
 ]
